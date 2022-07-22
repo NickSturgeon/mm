8aea511c3f615226fdec539a59e5ed01ebb8998e
Nicholas Sturgeon <sturgeon.nick@icloud.com>
2022-07-21 21:36:56 -0400

    Implement func_809613E8

--- src/overlays/actors/ovl_Obj_Mure2/z_obj_mure2.c
+++ src/overlays/actors/ovl_Obj_Mure2/z_obj_mure2.c
@@ -77,7 +77,9 @@ void func_809613C4(ObjMure2* this, PlayState* play) {
     func_809613E8(this);
 }
 
-#pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Mure2/func_809613E8.s")
+void func_809613E8(ObjMure2* arg0) {
+    arg0->actionFunc = func_809613FC;
+}
 
 #pragma GLOBAL_ASM("asm/non_matchings/overlays/ovl_Obj_Mure2/func_809613FC.s")
 
